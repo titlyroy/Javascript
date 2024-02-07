939a95f0c86041941b8c76da8a4680af437a65c6
Titly Roy <113987412+titlyroy@users.noreply.github.com>
2024-02-08 04:33:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,96 +1,84 @@
-let boxes= document.querySelectorAll(".box");
-let resetButton= document.querySelector("#reset-btn");
-let newButton=document.querySelector("#new-btn");
-let para= document.querySelector("#msg");
-let msgContainer= document.querySelector(".msg-container");
+let userScore=0;
+let compScore=0;
 
+let choices=document.querySelectorAll(".choice");
+let msg= document.querySelector("#msg");
+let userScoreboard=document.querySelector("#user-score");
+let compScoreboard=document.querySelector("#comp-score");
 
-//player x / player 0 trun added
-let turnx=true; 
-
-// add winning patterns in 2D array
-const winPatterns=[
-[0, 1, 2],
-[3, 4, 5],
-[6, 7, 8],
-[0, 3, 6],
-[1, 4, 7],
-[2, 5, 8],
-[0, 4, 8],
-[2, 4, 6]
-];
-
-
-//for reset the game-->
-const resetGame=()=>{
-    turnx=true;
-    enableBoxes();
-    msgContainer.classList.add("hide");
-
-
+//generate omputer choice , store all options in an array and using Math class for enerating random numbers and for getting whole numbers multiplying by more than one digit of the last index number. here last index= 2 so multiply with 3.
+const genCompChoice=()=>{
+    const options=["rock", "paper", "scissors"];
+    const  randonIndex=Math.floor(Math.random()*3);
+    return options[randonIndex];
 };
-
-//for x and 0 adding in the boxes and make it unchangable
-boxes.forEach((box)=>{
-     box.addEventListener("click",()=>{
-        // console.log("box was clicked");
-        if(turnx){
-            box.innerText="X";
-            turnx=false;
-        }else{
-            box.innerText="0";
-            box.style.color="#ff7900";
-            turnx=true;
-        }
-        box.disabled=true;//for not changing after clicked once
-
-        checkWinner();
-     });
+//draw game
+const drawGame=() =>{
+    msg.innerText="Game is draw! Play again.";
+    msg.style.backgroundColor="#012a4a";
     
-});
-//for disable all button after getting first winner
-const disableBoxes=(box)=>{
-    for(let box of boxes){
-        box.disabled=true;
-    }
-};
-// need to enable all buttons for reset or new game start
-const enableBoxes=(box)=>{
-    for(let box of boxes){
-        box.disabled=false;
-        box.innerText="";
-    }
-};
-//showing the winner in the top
-const showWinner=(winner)=>{
-    para.innerText=`Congradulations! The winner is ${winner}`;
-    msgContainer.classList.remove("hide");
-    disableBoxes();
 };
 
+//print user win
+const showWinner = (userWin,userChoice,compChoice) =>{
+    if(userWin){
 
-
-
-//checking winner need to access all the position of boxarray 
-const checkWinner = ()=>{
-    for(let pattern of winPatterns){
+        userScore++;
+        userScoreboard.innerText = userScore;
+        msg.innerText=`You win! your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor="green";
         
-          let pos1Val =  boxes[pattern[0]].innerText;
-          let pos2Val =  boxes[pattern[1]].innerText;
-          let pos3Val =  boxes[pattern[2]].innerText;
-
-// if there is no empty place then only it will check for the value is equal for 3 position or not
-
-   if(pos1Val !=="" && pos2Val !=="" && pos3Val !==""){
-    if(pos1Val===pos2Val&& pos2Val===pos3Val){
-    // console.log("winner", pos1Val);
-    showWinner(pos1Val);
    
+    }else{
+        compScore++;
+        compScoreboard.innerText = compScore;
+        msg.innerText=`You lose! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor="red";
+       
+        
+    }
+};
+
+
+
+//we are using modular functions=> means individual function will do a work.
+const playGame = (userChoice)=>{
+    // console.log("your choice =",userChoice);
+    //comp choice=>
+   const compChoice= genCompChoice();
+//    console.log("computer choice =",compChoice);
+   //logic for in and lose-->
+   
+   if(userChoice === compChoice){
+    drawGame();
    }
-}
-}};
+   else{
+    let userWin=true;
+    //paper , scissors
+    if(userChoice === "rock"){
+        userWin=compChoice==="paper"? false : true;
+    }
+    //rock,scissors
+    else if(userChoice==="paper"){
+        userWin=compChoice==="scissors"? false : true;
+    }
+    //rock,paper
+    else{
+        userWin=compChoice==="scissors"? false : true;
 
-//acceess the reset and new game button-->
-resetButton.addEventListener("click", resetGame);
-newButton.addEventListener("click", resetGame);
+    }
+    showWinner(userWin,userChoice,compChoice);
+   }
+   
 
+   
+   
+};
+// for click on every choices=>
+choices.forEach((choice)=>{
+ choice.addEventListener("click",()=>{
+const userChoice=choice.getAttribute("id");
+playGame(userChoice);
+ });
+
+});
